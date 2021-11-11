a09a2807d714439a6830118c0bcb8f7e570fd9f5
Michał Płużek <michalpluzek@gmail.com>
2021-11-11 11:54:30 +0100

    Add methods to todo service

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -8,4 +8,20 @@ export class DataService {
   todos: Todo[] = [];
 
   constructor() {}
+
+  getAllTodos() {
+    return this.todos;
+  }
+
+  addTodo(todo: Todo) {
+    this.todos.push(todo);
+  }
+
+  updateTodo(index: number, updatedTodo: Todo) {
+    this.todos[index] = updatedTodo;
+  }
+
+  deleteTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
 }

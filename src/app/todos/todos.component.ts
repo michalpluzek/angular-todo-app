e1a52aca9a989f9e00115e18e229b71ed884a1c9
Michał Płużek <michalpluzek@gmail.com>
2021-11-11 13:48:24 +0100

    Add ngForm and form validation

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../shared/data.service';
 import { Todo } from '../shared/todo.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todos',
@@ -9,10 +10,20 @@ import { Todo } from '../shared/todo.model';
 })
 export class TodosComponent implements OnInit {
   todos: Todo[] = [];
+  showValidationErrors: boolean = false;
 
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.todos = this.dataService.getAllTodos();
+  }
+
+  onFormSubmit(form: NgForm) {
+    if (form.invalid) {
+      this.showValidationErrors = true;
+      return;
+    }
+    this.dataService.addTodo(new Todo(form.value.text));
+    form.reset();
   }
 }

0435e09fcd0ea5ea0b1d71b147cec64bf16748ba
Michał Płużek <michalpluzek@gmail.com>
2021-11-11 11:51:01 +0100

    Add model todo and service data

---
+++ src/app/shared/data.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  todos: Todo[] = [];
+
+  constructor() {}
+}
